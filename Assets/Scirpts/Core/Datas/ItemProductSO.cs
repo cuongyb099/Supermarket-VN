fdb073e5b5f74d5e404be68f4c654bbbcbbcea00
Dung <vuquocdung204@gmail.com>
2025-06-03 13:04:40 +0700

    item product base

--- Assets/Scirpts/Core/Datas/ItemProductSO.cs
+++ Assets/Scirpts/Core/Datas/ItemProductSO.cs
@@ -2,51 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemProduct_Type
-{
-    None = 0,
-    Food = 1,
-    Tech = 2,
-}
-
-
 [CreateAssetMenu(menuName = "Item Config")]
 public class ItemProductSO : ScriptableObject
 {
-    public List<ItemFood> lsFoods;
-    public List<ItemTech> lsTechs;
+    public List<ItemProductBase> lsItemProducts;
 
-
-    public ItemFood GetItemFoodById(string id)
+    public ItemProductBase GetItemProductById(int idProduct)
     {
-        foreach(var food in this.lsFoods) if(food.id == id) return food;
-        return null;
-    }
-
-    public ItemTech GetItemTechById(string id)
-    {
-        foreach (var tech in this.lsTechs) if (tech.id == id) return tech;
+        foreach(var item in this.lsItemProducts) if(item.idProduct == idProduct) return item;
         return null;
     }
 }
 
 [System.Serializable]
-public class ItemProduct
+public class ItemProductBase
 {
-    public string id;
-    public string name;
+    public int idProduct;
+    public string nameProduct;
+    public Vector3 size;
+    public int amountPerBox;
     public float sellPrice;
     public float buyPrice;
-    public ItemProduct_Type itemType;
-}
-[System.Serializable]
-public class ItemFood : ItemProduct
-{
-
-}
-
-[System.Serializable]
-public class ItemTech: ItemProduct
-{
-
+    public int boxRef;
 }
